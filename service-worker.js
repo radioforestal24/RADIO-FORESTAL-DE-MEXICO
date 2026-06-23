298a41f3393e95a9316b6049d973ba40fcfd9226
radioforestal <alfredo_11gc@yahoo.com.mx>
2026-06-23 16:28:36 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,27 @@
-self.addEventListener("install", () => {
-self.skipWaiting();
+const CACHE_NAME = "radio-forestal-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/radio.html",
+"/noticias.html",
+"/contacto.html",
+"/manifest.json",
+"/icon.png"
+];
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(fetch(event.request).catch(() => {
-return caches.match(event.request);
-}));
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
 });
